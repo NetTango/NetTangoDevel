083b895c52a9384ab573f63cf49a82aa48a4c6a1
Arthur Hjorth <hermeshjorth2011@u.northwestern.edu>
2012-09-25 14:57:08 -0500

    Background image drawing added to models

---
+++ java/ntango/NetTangoXMLParser.java
@@ -0,0 +1,13 @@
+package ntango;
+
+
+import javax.xml.parsers.DocumentBuilder;
+
+public class NetTangoXMLParser {
+	protected DocumentBuilder docBuilder;
+	
+	public NetTangoXMLParser ()
+	{
+		
+	}
+}
